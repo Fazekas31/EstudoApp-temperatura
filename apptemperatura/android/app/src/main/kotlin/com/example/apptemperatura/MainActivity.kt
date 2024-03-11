bc2f4a91a1c7bdd0ecd466b4d770172bc1f7a4d5
Leonardo Fazekas <leonardofazekas@gmail.com>
2024-03-10 21:25:36 -0300

    Interface

---
+++ apptemperatura/android/app/src/main/kotlin/com/example/apptemperatura/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.apptemperatura
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ apptemperatura/lib/pages/home.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 53, 4, 152),
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: double.infinity,
+          child: Padding(
+            padding: EdgeInsets.only(top: 80, left: 10, right: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  'Conversor de temperatura',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 212, 177, 5),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 25,
+                  ),
+                ),
+                Text(
+                  'Converta em Celsius para Fahrenheit',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 15,
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Entre com uma temp em Celsius'),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Temperatura em Fahrenheit'),
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.normal),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    child: Text('Converter'),
+                  ),
+                ),
+                 SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: ElevatedButton(
+                    onPressed: null,
+                    child: Text('Limpar'),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
